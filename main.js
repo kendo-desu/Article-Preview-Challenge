5e2cbae319f4da4b2e08578ebb0679f4f25add1f
kendo-desu <135325323+kendo-desu@users.noreply.github.com>
2024-07-05 22:45:40 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', function (e) {
+    //console.log(e.target);
+    //console.log(e.currentTarget);
+    const currentColor = button.style.backgroundColor;
+    button.style.backgroundColor = 
+  })
